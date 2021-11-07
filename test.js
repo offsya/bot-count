ce96d81432b7def893886fb7995ae97d036c133e
Yura <idblablobla@gmail.com>
2021-11-07 14:36:58 +0300

    'testy'

--- test.js
+++ test.js
@@ -5,9 +5,10 @@ exec("git add .", (error) => {
         console.log(`error: ${error}`);
         return;
     }
+    console.log('please')
     console.log(`git add .`);
 });
-exec("git commit -m 'test'", (error) => {
+exec("git commit -m 'test1'", (error) => {
     if (error) {
         console.log(`error: ${error}`);
         return;
